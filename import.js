7006472b900081c9dd209b471d4f011e0db26720
Deepa <45240469+deepa2986@users.noreply.github.com>
2023-02-09 09:22:58 +0530

    Add files via upload

---
+++ import.js
@@ -0,0 +1,8 @@
+import * as utils from "./export.js"
+
+console.log("Principal Amount : " ,utils.principal)
+console.log("Interest rate : " ,utils.interestRate)
+console.log("Time : " ,utils.time)
+
+utils.moneyInterestCalculate()
+
