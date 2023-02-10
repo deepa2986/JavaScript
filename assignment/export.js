4a7f13f0bbfd4eacc3e82990e551ab81f068c71e
Deepa <45240469+deepa2986@users.noreply.github.com>
2023-02-10 11:14:43 +0530

    Add files via upload

---
+++ assignment/export.js
@@ -0,0 +1,11 @@
+export function m1(){
+    console.log("m1 fuction")
+}
+
+export const id = 100
+
+export let obj = {name : "asda" , value : 200000}
+
+function m2(){
+    console.log("m2 function")
+}
