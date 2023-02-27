23d062faec2103a2d2ec88a5a9b153b9a27c0882
Deepa <45240469+deepa2986@users.noreply.github.com>
2023-02-27 14:16:48 +0530

    Add files via upload

--- Assessment-Product-App/src/App.js
+++ Assessment-Product-App/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Products from './components/Products';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
       <h3>Products-App</h3>
+      <ProductList />
       <Products />
     </div>
   );
--- Assessment-Product-App/src/components/ProductList.js
+++ Assessment-Product-App/src/components/ProductList.js
@@ -7,7 +7,7 @@ import Products from './Products';
             "title":"Brown Eggs",
             "type":"Eggs",
             "description":"Raw organic brown eggs in a basket",
-            "url":"https://backyardpoultry.iamcountryside.com/eggs-meat/top-15-best-brown-egg-layers/",
+            "url":"https://tse2.mm.bing.net/th/id/OIP.xc8KFVlFSUe3nqj5q18wQAHaE8?pid=ImgDet&rs=1ttps:",
             "price":"28.1",
             "rating":4
         },
@@ -15,7 +15,7 @@ import Products from './Products';
             "title":"Sweet fresh stawberry",
             "type":"Dairy",
             "description":"Sweet fresh stawberry on the wooden table",
-            "url":"https://www.bing.com/images/search?view=detailV2&ccid=ijhzQC4V&id=C8AA43291A2ACEF2533009C9C7C033FCE483ED79&thid=OIP.ijhzQC4Vuon5frscpws6lAHaHK&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.8a3873402e15ba89f97ebb1ca70b3a94%3frik%3dee2D5PwzwMfJCQ%26riu%3dhttp%253a%252f%252fwww.figtreeolivebranch.com%252fwp-content%252fuploads%252f2013%252f12%252fstrawberry-03.jpg%26ehk%3dRQgjX4a%252fOIrp5eiOff53rx%252b2isi2PxVu058bNrPvPB4%253d%26risl%3d1%26pid%3dImgRaw%26r%3d0&exph=3358&expw=3469&q=strawberry&simid=607990640274258004&FORM=IRPRST&ck=44444C9A9DE950A7B81A5A1E06824B59&selectedIndex=1",
+            "url":"https://th.bing.com/th/id/R.112bedb019d8c563b8d00d3ad4cb49a0?rik=%2fahRquIPkseC%2fw&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f34900000%2fStrawberry-fruit-34914839-1920-1200.jpg&ehk=ARGJa8ucdVWm9KqfniVZfr6amUWNdvDxBJnvMqoFEeE%3d&risl=&pid=ImgRaw&r=0",
             "price":"29.45",
             "rating":5
         },
